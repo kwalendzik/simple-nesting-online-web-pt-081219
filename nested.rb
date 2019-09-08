04a24a37c0c5db8496a6dfd1c16857856325fb20
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-08 18:38:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
+     
 programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -36,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+	programmer_hash[:alan_kay][:known_for]
+	
 end
 
 def dennis_ritchies_language
@@ -54,6 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages]
+
 end
 
 def adding_matz
